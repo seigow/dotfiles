391dd05423b4326fed44287fb78834f72e2cc39d
seigow <se.fuku323@gmail.com>
2015-08-14 16:14:02 +0900

    modifi '.zshrc' and add '.tmux.conf'

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/colors ~/.vim
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
--- mklink.bat
+++ mklink.bat
@@ -1,3 +1,4 @@
 mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles\.vimrc"
 mklink /D %HOMEPATH%"\.vim\colors" %HOMEPATH%"\dotfiles\.vim\colors"
+mklink %HOMEPATH%"\.tmux.conf" %HOMEPATH%"\dotfiles\.tmux.conf"
 exit 0
