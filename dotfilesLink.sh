e611605afb6317d08eb3be84f60245a7a6bd9a6a
seigow <seigow.fukushima@gmail.com>
2019-05-13 11:34:32 +0900

    add xonshrc and gitconfig to init script

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -6,3 +6,5 @@ ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.zshenv ~/.zshenv
+ln -sf ~/dotfiles/.xonshrc ~/.xonshrc
+ln -sf ~/dotfiles/.gitconfig  ~/.gitconfig
