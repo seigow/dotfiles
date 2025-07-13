a9f7d456cb8354a43eb0343b1778da968a396550
seigow <seigow.fukushima@gmail.com>
2025-07-13 15:26:51 +0900

    Remove unused files

--- mklink.bat
+++
@@ -1,5 +0,0 @@
-mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles\.vimrc"
-mklink /D %HOMEPATH%"\.vim\colors" %HOMEPATH%"\dotfiles\.vim\colors"
-mklink /D %HOMEPATH%"\.vim\ftplugin" %HOMEPATH%"\dotfiles\.vim\ftplugin"
-mklink %HOMEPATH%"\.tmux.conf" %HOMEPATH%"\dotfiles\.tmux.conf"
-exit 0
